aa0ac30edd66d77464e69b54b4476f5e9b89ad46
shinhonggyu <zowoz0@naver.com>
2022-02-03 14:56:24 +0900

    UI complete

---
+++ src/components/TodoHead.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+const TodoHeadBlock = styled.div`
+  padding-top: 48px;
+  padding-right: 32px;
+  padding-bottom: 24px;
+  padding-left: 32px;
+  border-bottom: 1px solid #e9ecef;
+
+  h1 {
+    margin: 0;
+    font-size: 36px;
+    color: #343a40;
+  }
+
+  .day {
+    margin-top: 4px;
+    color: #868e96;
+    font-size: 21px;
+  }
+
+  .tasks-left {
+    margin-top: 40px;
+    color: #20c997;
+    font-size: 18px;
+    font-weight: bold;
+  }
+`;
+
+function TodoHead() {
+  return (
+    <TodoHeadBlock>
+      <h1>2022년 2월 3일</h1>
+      <div className="day">수요일</div>
+      <div className="tasks-left">할 일 2개 남음</div>
+    </TodoHeadBlock>
+  );
+}
+
+export default TodoHead;
